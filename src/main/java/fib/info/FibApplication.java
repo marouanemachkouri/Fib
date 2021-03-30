f85f2175b3330b680a71d5017aec8e15b9914ca2
marouanemachkouri <mar6mach@gmail.com>
2021-03-30 17:03:30 +0100

    Fib

---
+++ src/main/java/fib/info/FibApplication.java
@@ -0,0 +1,13 @@
+package fib.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibApplication.class, args);
+	}
+
+}
